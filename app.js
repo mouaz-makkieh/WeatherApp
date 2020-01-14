f20d29e980eacb84988d0dae9748a7bb5fb6a40b
User0008 <mouaz-makkieh>
2020-01-14 14:43:04 -0500

    show  error message

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // api key : 82005d27a116c2880c8f0fcb866998a0
+let latitude;
+let longitude;
+const notification = document.getElementsByClassName("notification")[0];
+
+function getLocation() {
+  if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+function onSuccess(position) {
+  console.log(position);
+}
+
+function onError(error) {
+  console.log("sharmota", error);
+  const p = document.createElement("p");
+  p.innerHTML = error.message;
+  notification.style.display = "block";
+  notification.appendChild(p);
+}
+
+getLocation();
