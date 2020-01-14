e3a090b2b156bec08546a33f4ad79b4915386d0a
User0008 <mouaz-makkieh>
2020-01-14 15:55:54 -0500

    new changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// api key : 82005d27a116c2880c8f0fcb866998a0
+const apiKey = "82005d27a116c2880c8f0fcb866998a0";
 let latitude;
 let longitude;
 const notification = document.getElementsByClassName("notification")[0];
@@ -10,6 +10,18 @@ function getLocation() {
 
 function onSuccess(position) {
   console.log(position);
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+      latitude +
+      "&lon=" +
+      longitude +
+      "&appid=" +
+      apiKey
+  )
+    .then(response => response.json())
+    .then(weather => console.log(weather));
 }
 
 function onError(error) {
